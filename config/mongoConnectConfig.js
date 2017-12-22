5e276e170b5f475b073b804cb30c4e23ab05fd3a
ashwini <ashwinidx@gmail.com>
2017-12-22 21:45:22 +0530

    ad

--- config/mongoConnectConfig.js
+++ config/mongoConnectConfig.js
@@ -1,1 +1,1 @@
-export const MONGO_CONFIG = 'mongodb:https://obscure-wildwood-56958.herokuapp.com:27017/oneCrore'
+export const MONGO_CONFIG = 'mongodb://obscure-wildwood-56958.herokuapp.com:27017/oneCrore'
