ee338dfb7596bea136fc3fa32261d863261e3833
ashwini <ashwinidx@gmail.com>
2017-12-24 20:57:11 +0530

    dd

--- config/mongoConnectConfig.js
+++ config/mongoConnectConfig.js
@@ -1,3 +1,3 @@
-export const MONGOLAB_URI = 'mongodb://ashwinik:qwerty@/localhost:27017/oneCrore';
+export const MONGOLAB_URI = 'mongodb://ashwinik:qwerty@localhost:27017/oneCrore';
 
 //export const MONGO_CONFIG = 'mongodb://localhost:27017/oneCrore';
